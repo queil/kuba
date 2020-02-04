6f89b16a5b0b02b3e57aca6cc2eb29e67f9cb6b7
queil <queil@users.noreply.github.com>
2020-02-04 21:45:19 +0000

    Added basic commands

--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,41 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-
-	let disposable = vscode.commands.registerCommand('kuba.pickPod', () => {
+	async function kubectl(argsString: string) {
+		const util = require('util');
+		const exec = util.promisify(require('child_process').exec);
 		
-		vscode.window.showInformationMessage('Hello World!');
-	});
+		const { stdout, stderr } = await exec('kubectl.exe' + ' ' + argsString + ' ' + '-o jsonpath="{range .items[*]}{.metadata.name}{\',\'}{end}"');
+		if (stderr) {
+			vscode.window.showErrorMessage(stderr);
+		}
+		return stdout.split(",");
+	}
 
-	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(vscode.commands.registerCommand('kuba.pickPod', async () => {
+		
+		const config = vscode.workspace.getConfiguration('kuba');
+		const namespace = config.get<string>("namespace");
+
+		if (!namespace) {
+			await vscode.commands.executeCommand('kuba.useNamespace');
+		}
+
+		const selectedPod = await vscode.window.showQuickPick(kubectl("get pod"), { placeHolder: 'Pick pod...' });
+
+		config.update("pod", selectedPod);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('kuba.useNamespace', async () => {
+		
+		const selectedNs = await vscode.window.showQuickPick(kubectl("get namespaces"), { placeHolder: 'Pick namespace...' });
+
+		const config = vscode.workspace.getConfiguration('kuba');
+		config.update("namespace", selectedNs);
+	}));
+
+	
 }
 
 export function deactivate() {}
