e7166dc037860ff1eb1e8fbeced1cb22562326cb
queil <queil@users.noreply.github.com>
2020-03-02 08:40:15 +0000

    Fixed offset bug in LogReader

--- src/logReader.ts
+++ src/logReader.ts
@@ -10,17 +10,17 @@ export class LogReader {
         this.FileName = fileName;
     }
     public Stream = (callback: (line:string) => void) =>
-    {   let offset = 1;
+    {   
+        let offset = 0;
         const s = 
             fs.createReadStream(this.FileName)
               .pipe(es.split())
-              .pipe(es.filterSync((_:string) => { return offset > this.TotalOffset; }))
+              .pipe(es.filterSync((_:string) => { return offset++ >= this.TotalOffset; }))
               .pipe(
                 es.mapSync((ln:string) => {
                     s.pause();
                     callback(ln);
                     this.TotalOffset++;
-                    offset++;
                     s.resume();
                 }).on('error', (err:string) => { console.log('Error while reading file.', err); })
                   .on('end', () => { console.log('Read entire file.'); })
